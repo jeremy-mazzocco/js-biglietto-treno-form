36e50ec74ac5c6e0af91eb2f27f66a3572b8b729
jeremy-mazzocco <jeremyozzg@gmail.com>
2023-05-02 08:36:39 +0100

    laast fixes

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-// varibili globali
+// Varibili globali
 let agePrice, userAge, userName, quantityKilometers;
 
 
-// al click scrivi ti HTML
+// Button - al click scrivi calcola prezzo biglietto
 const button = document.getElementById('myBtn').addEventListener('click',
     function () {
         // Salva chilometri e eta' in variabili
@@ -31,15 +31,34 @@ const button = document.getElementById('myBtn').addEventListener('click',
     });
 
 
-// al click cancel data
+// Button - al click cancel data
 const buttonCancel = document.getElementById('myBtn-cancel').addEventListener('click',
     function () {
         // scrivi sull'HTML
         userName = document.getElementById("input_name").value = " ";
         userAge = document.getElementById("input_age").value = " ";
         quantityKilometers = document.getElementById("input_kilometers").value = " ";
-    }
-)
+    });
 
 
+// Button - win a free ticket
+const buttonLuck = document.getElementById('myBtn-luck').addEventListener('click',
+    function () {
+        // onclick generate a random number
+        const giveRdm = Math.floor(Math.random() * 11);
+        // add display: block to a div
+        const luckDisplay = document.querySelector(".win").style.display = "block";
+        let luckText = document.getElementById('text-win');
 
+        // from 0 to 1 you win, from 2 to 7 you don't win, if 8 to 10 pay double
+        if (giveRdm <= 1) {
+            luckText.innerHTML = "You WIN a FREE trip !!!";
+        } else if ((giveRdm >= 2) && (giveRdm <= 7)) {
+            luckText.innerHTML = "It's not your lucky day, pay the same";
+        } else {
+            luckText.innerHTML = ":-P you pay double";
+        }
+    });
+
+
+// check if price is calulate then yu can play the game
